62f7d1c62b16d4011b45f978e8274294b723d9bc
Shyamali8 <109735121+Shyamali8@users.noreply.github.com>
2022-09-20 15:55:35 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,14 @@
+
+public class Vowels 
+{
+	public static void main(String... g)
+	{
+		 
+		char a='I';
+		
+         // ternary operator    (condition)?(true):(false)		
+		
+		System.out.println((a=='A'  ||  a=='E'  || a=='I' ||  a=='O' ||  a=='U')?"Vowel" :"Consonant");
+	}
+
+}
